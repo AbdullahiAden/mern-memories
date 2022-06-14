eea92e4c1c035cc35b34bf57b9fb4546ed9f734d
Abbe <abbe@Abbe.local>
2022-06-14 18:57:21 +0200

    changed import to require

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
