fb257e0d1ab562061f4bffe5f466d195ad2bee5c
Abbe <abbe@Abbe.local>
2022-06-19 12:38:27 +0200

    backend setup

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/posts", require("./routes/postRoutes"));
+app.use(errorHandler);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
