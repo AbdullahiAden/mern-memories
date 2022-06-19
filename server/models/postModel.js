2f9d44e05d10f75275ffed20bc412fd8689c3b9c
Abbe <abbe@Abbe.local>
2022-06-19 12:36:11 +0200

    post model

---
+++ server/models/postModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFiile: String,
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post");
