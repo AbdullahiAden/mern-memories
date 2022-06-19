5298ac498201f4ce59572b238b190058ce6ded2e
Abbe <abbe@Abbe.local>
2022-06-19 12:35:28 +0200

    db setup

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.CONNECTION_URL);
-    console.log("mongodb connected");
+    console.log(`mongodb connected : ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     // exit code 1 is for failure
