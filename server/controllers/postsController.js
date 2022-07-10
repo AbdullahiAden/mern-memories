6ceacf4b29298744959d7f342e62f1f75c14e5e0
Abbe <abbe@Abbe.local>
2022-07-10 15:28:11 +0200

    create post

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,10 +1,21 @@
 const asyncHandler = require("express-async-handler");
 
+const Post = require("../models/postModel");
+
 const getPosts = asyncHandler(async (req, res) => {
-  res.status(200).send("all posts");
+  const posts = await Post.find();
+  res.status(200).json(posts);
 });
+
 const createPost = asyncHandler(async (req, res) => {
-  res.status(200).send("all posts");
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error("please provide a title");
+  }
+  const post = await Post.create({
+    title: req.body.title,
+  });
+  res.status(200).json(post);
 });
 
 module.exports = {
